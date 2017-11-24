934c2d194cde037d258fbe06d8d454d9fde7ca41
Vitalii_Kozak <vitalii-kozak@ukr.net>
2017-11-24 17:00:46 +0200

    24.11.2017

--- src/main/java/service/ParcerSaxXML.java
+++ src/main/java/service/ParcerSaxXML.java
@@ -5,6 +5,18 @@ import domain.Currency;
 import java.io.InputStream;
 import java.util.List;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
 public class ParcerSaxXML implements Parser {
 
 
