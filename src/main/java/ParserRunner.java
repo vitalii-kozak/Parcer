0be17f0e11d15b8cabc766a363527f3c67bafea6
Desktop <vitalii-kozak@ukr.net>
2017-11-27 00:40:23 +0200

    27.11.2017

--- src/main/java/ParserRunner.java
+++ src/main/java/ParserRunner.java
@@ -22,27 +22,27 @@ public class ParserRunner {
         parcerTypes.put(Service.HTML, new ParcerHTML());
 
         Parser parcer = parcerTypes.get(Service.DOMXML);
-//        System.out.println("*1111111111111111111111111111111111111111111111111111111111*");
-//
-//        try {
-//            URL url = new URL("http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
-//            URLConnection connection = url.openConnection();
-//            connection.getURL();
-//            connection.addRequestProperty("User-Agent", "Mozilla/5.0");
-//
-//            List<Currency> curs = parcer.parse(connection.getInputStream());
-//
-//            System.out.println(curs);
-//            for (Currency value: curs
-//                 ) {
-//                if (value.getCode() == 840) {
-//                    System.out.println(value);
-//                }
-//            }
-//
-//        } catch ( Exception e ) {
-//            e.printStackTrace();
-//        }
+        System.out.println("*1111111111111111111111111111111111111111111111111111111111*");
+
+        try {
+            URL url = new URL("http://bank.gov.ua/NBUStatService/v1/statdirectory/exchange");
+            URLConnection connection = url.openConnection();
+            connection.getURL();
+            connection.addRequestProperty("User-Agent", "Mozilla/5.0");
+
+            List<Currency> curs = parcer.parse(connection.getInputStream());
+
+            System.out.println(curs);
+            for (Currency value: curs
+                 ) {
+                if (value.getCode() == 840) {
+                    System.out.println(value);
+                }
+            }
+
+        } catch ( Exception e ) {
+            e.printStackTrace();
+        }
 
         parcer = parcerTypes.get(Service.SAXXML);
         System.out.println("*222222222222222222222222222222222222222222222222222222222222*");
